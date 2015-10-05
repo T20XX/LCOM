e8eb11e4421a551275d71588606c1930ea5f9805
Telmo Barros <T20XX@users.noreply.github.com>
2015-10-05 10:09:25 +0000

    Lab 2.1

--- lab2/timer.c
+++ lab2/timer.c
@@ -1,5 +1,6 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
+#include "i8254.h"
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
@@ -21,12 +22,17 @@ void timer_int_handler() {
 }
 
 int timer_get_conf(unsigned long timer, unsigned char *st) {
-	
-	return 1;
+	unsigned char tempbyte;
+	tempbyte = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+	sys_outb(TIMER_CTRL, tempbyte);
+	unsigned long aux;
+	sys_inb(timer,&aux);
+	*st = aux;
+	return *st;
 }
 
 int timer_display_conf(unsigned char conf) {
-	
+	printf("0x%x \n",conf);
 	return 1;
 }
 
@@ -41,6 +47,10 @@ int timer_test_int(unsigned long time) {
 }
 
 int timer_test_config(unsigned long timer) {
+	unsigned char *tmp;
+	unsigned char conf;
+	conf = timer_get_conf(timer,tmp);
+	timer_display_conf(conf);
 	
 	return 1;
 }
