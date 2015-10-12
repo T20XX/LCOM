82f9d0b0c3d7cd94daa530ddca676c570831946c
Telmo Barros <T20XX@users.noreply.github.com>
2015-10-12 08:27:10 +0000

    test_int in progress

--- lab2/timer.c
+++ lab2/timer.c
@@ -2,6 +2,11 @@
 #include <minix/drivers.h>
 #include "i8254.h"
 
+int counter;
+int irq_set;
+int hook_id;
+int timer0_id;
+
 int timer_set_square(unsigned long timer, unsigned long freq) {
 	unsigned char conf, tmp, MSB, LSB;
 	unsigned long div;
@@ -25,18 +30,21 @@ int timer_set_square(unsigned long timer, unsigned long freq) {
 	return 0;
 }
 
-int timer_subscribe_int(void ) {
-
-	return 1;
+int timer_subscribe_int() {
+	timer0_id;
+	hook_id = timer0_id;
+	sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id);
+	sys_irqenable(&hook_id);
+	return 0;
 }
 
 int timer_unsubscribe_int() {
-
-	return 1;
+	sys_irqrmpolicy(&hook_id);
+	return 0;
 }
 
 void timer_int_handler() {
-
+	counter++;
 }
 
 int timer_get_conf(unsigned long timer, unsigned char *st) {
@@ -80,8 +88,37 @@ int timer_test_square(unsigned long freq) {
 }
 
 int timer_test_int(unsigned long time) {
-	
-	return 1;
+	timer_subscribe_int();
+	timer_unsubscribe_int();
+
+	int ipc_status;
+	message msg;
+	int r;
+
+	counter = 0;
+
+while( counter < time ) { /* You may want to use a different condition */
+	/* Get a request message. */
+	if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+		printf("driver_receive failed with: %d", r);
+		continue;
+	}
+	if (is_ipc_notify(ipc_status)) { /* received notification */
+		switch (_ENDPOINT_P(msg.m_source)) {
+		case HARDWARE: /* hardware interrupt notification */
+			if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+				timer_int_handler();  /* process it */
+				printf ("teste");
+			}
+			break;
+		default:
+			break; /* no other notifications expected: do nothing */
+		}
+	} else { /* received a standard message, not a notification */
+		/* no standard messages expected: do nothing */
+	}
+}
+	return 0;
 }
 
 int timer_test_config(unsigned long timer) {
