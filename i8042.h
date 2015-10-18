bb75ef247cc98275cf18b502388b210f535c1b27
Telmo Barros <T20XX@users.noreply.github.com>
2015-10-18 18:42:03 +0000

    Import of lab3 with makefile and conf

---
+++ i8042.h
@@ -0,0 +1,15 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+/** @defgroup i8042 i8042
+ * @{
+ *
+ * Constants for i8042 KEYBOARD CONTROLLER
+ */
+
+
+
+
+/**@}*/
+
+#endif /* _LCOM_I8042_H */
