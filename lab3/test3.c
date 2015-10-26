6ee9567e09394b0ca708d533da1565cbb3a4b01d
Telmo Barros <T20XX@users.noreply.github.com>
2015-10-26 00:25:53 +0000

    Lab3 preparation

--- lab3/test3.c
+++ lab3/test3.c
@@ -93,5 +93,42 @@ int kbd_test_leds(unsigned short n, unsigned short *leds) {
 }
 
 int kbd_test_timed_scan(unsigned short n) {
-    /* To be completed */
+	 timer_subscribe_int();
+
+	   	int  timer_irq_set = timer_subscribe_int();
+	   	int  kbd_irq_set = kbd_subscribe_int();
+	   	int ipc_status;
+	   	message msg;
+	   	int r;
+
+	   	int counter = 0; //Inicialização do contador
+
+	   	while( counter < (n * 60) && code != BREAKCODE) { /* You may want to use a different condition */
+	   		/* Get a request message. */
+	   		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+	   			printf("driver_receive failed with: %d", r);
+	   			continue;
+	   		}
+	   		if (is_ipc_notify(ipc_status)) { /* received notification */
+	   			switch (_ENDPOINT_P(msg.m_source)) {
+	   			case HARDWARE: /* hardware interrupt notification */
+	   				if (msg.NOTIFY_ARG & kbd_irq_set) { /* subscribed interrupt */
+	   					counter = 0;
+	   					kbd_int_handler();
+		   				break;
+	   				}
+	   				if (msg.NOTIFY_ARG & timer_irq_set) { /* subscribed interrupt */
+	   					counter++;
+
+	   				}
+	   			default:
+	   				break; /* no other notifications expected: do nothing */
+	   			}
+	   		} else { /* received a standard message, not a notification */
+	   			/* no standard messages expected: do nothing */
+	   		}
+	   	}
+	   	timer_unsubscribe_int();
+	   	kbd_unsubscribe_int();
+	   	return 0;
 }
