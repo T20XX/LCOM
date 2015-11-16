6bf9ebe9831f9e8ebb0a660d82b24c16eb7418d8
Telmo Barros <T20XX@users.noreply.github.com>
2015-11-16 03:26:01 +0000

    test_init() working but stays in graphic mode

--- lab5/vbe.c
+++ lab5/vbe.c
@@ -11,10 +11,34 @@
 #define PB2OFF(x) ((x) & 0x0FFFF)
 
 int vbe_get_mode_info(unsigned short mode, vbe_mode_info_t *vmi_p) {
-  
-  /* To be completed */
-  
-  return 1;
+
+	phys_bytes buf;
+	struct reg86u r;
+	mmap_t map;
+
+	lm_init();
+	lm_alloc(sizeof(vbe_mode_info_t), &map);
+
+	 r.u.w.ax = GET_VBE_INFO; /* VBE get mode info */
+	/* translate the buffer linear address to a far pointer */
+	r.u.w.es = PB2BASE(buf); /* set a segment base */
+	r.u.w.di = PB2OFF(buf); /* set the offset accordingly */
+	r.u.w.cx = mode;
+	r.u.b.intno = VIDEO_CARD;
+
+
+	if( sys_int86(&r) != OK ) { /* call BIOS */
+		lm_free(&map);
+		return 1;
+	}
+	else
+	{
+		*vmi_p = *((vbe_mode_info_t*) map.virtual);
+		lm_free(&map);
+		return 0;
+	}
+
+
 }
 
 
