8d94ed576a332bf6538412c0de7429e37e2ba8ae
up201405840 <up201405840@8b2b798f-491d-4078-857d-234f47bdacfb>
2015-11-29 02:14:30 +0000

    game class and new pieces

--- proj/game.h
+++ proj/game.h
@@ -1,4 +1,14 @@
 #ifndef __GAME_H
 #define __GAME_H
 
+#include "piece.h"
+
+typedef struct {
+	unsigned int game_mode;
+	Piece * actual_piece;
+	Piece * next_piece;
+} Game;
+
+Game * new_game(unsigned int mode);
+
 #endif /* __GAME_H */
