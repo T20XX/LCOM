30cc9a52855ca95afad9b5c52afba01d852090f2
Telmo Barros <T20XX@users.noreply.github.com>
2015-11-29 00:56:32 +0000

    One more module that will be necessary and liblm.a

--- proj/handler.c
+++ proj/handler.c
@@ -1,5 +1,9 @@
 #include "handler.h"
 
+typedef enum {MAIN_MENU, GAME_MODE1, GAME_MODE2,GAME_MODE3,GAME_MODE4,HIGH_SCORES,EXIT} state;
+typedef enum {CLICK_BUTTON1, CLICK_BUTTON2, CLICK_BUTTON3, CLICK_BUTTON4, CLICK_BUTTON5, CLICK_BUTTON6} event;
+
 int mainhandler(){
+
 	return 0;
 }
