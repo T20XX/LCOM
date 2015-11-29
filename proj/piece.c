4e7c159f6e227ce9fc4262f5deb83e77c3d04fac
Jorge Vale <jorge-vale1@hotmail.com>
2015-11-29 03:02:04 +0000

    if conditions done on piece

--- proj/piece.c
+++ proj/piece.c
@@ -10,25 +10,25 @@ Piece *new_piece(unsigned int xi, unsigned int yi, int yspeed){
 	piece->sprite.xspeed = 0;
 	piece->sprite.yspeed = yspeed;
 	if(random == 0){
-		piece->sprite.map = read_xpm(stairs, &tempwidth, &tempheight);
+		piece->sprite.map = read_xpm(stairs, &tempwidth, &tempheight); // WARNING
 	}
 	else if(random == 1){
-
+		piece->sprite.map = read_xpm(four, &tempwidth, &tempheight);
 	}
 	else if(random == 2){
-
+		piece->sprite.map = read_xpm(square, &tempwidth, &tempheight);
 	}
 	else if(random == 3){
-
+		piece->sprite.map = read_xpm(L, &tempwidth, &tempheight);
 	}
 	else if(random == 4){
-
+		piece->sprite.map = read_xpm(L_reverse, &tempwidth, &tempheight);
 	}
 	else if(random == 5){
-
+		piece->sprite.map = read_xpm(upstairs, &tempwidth, &tempheight);
 	}
 	else if(random == 6){
-
+		piece->sprite.map = read_xpm(downstairs, &tempwidth, &tempheight);
 	}
 
 	piece->sprite.width = tempwidth;
