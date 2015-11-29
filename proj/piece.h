bafe46d555b16031b49342de72bfe5a52f937012
Jorge Vale <jorge-vale1@hotmail.com>
2015-11-29 01:51:01 +0000

    piecec done

--- proj/piece.h
+++ proj/piece.h
@@ -1,4 +1,12 @@
 #ifndef __PIECE_H
 #define __PIECE_H
 
+#include "sprite.h"
+#include "piecespix.h"
+
+typedef struct{
+	Sprite sprite;
+} Piece;
+
+Piece *new_piece(unsigned int xi, unsigned int yi,int yspeed);
 #endif /* __PIECE_H */
