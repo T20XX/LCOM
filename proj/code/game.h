79f5e625cfe5df7ed8069d4ddecfdaba0deec2ca
up201405840 <up201405840@8b2b798f-491d-4078-857d-234f47bdacfb>
2015-12-05 15:40:18 +0000

    keyboard already subscribing and game machine state working, structure of files changes as requested

--- proj/code/game.h
+++ proj/code/game.h
@@ -4,7 +4,7 @@
 #include "piece.h"
 #include <stdio.h>
 
-typedef enum {FALL, MOVE_RIGHT, MOVE_LEFT, FALL_QUICKER, ROTATE, SWAP_PIECES ,REACH_END} State;
+typedef enum {FALL, MOVE_RIGHT, MOVE_LEFT, ROTATE, SWAP_PIECES ,REACH_END, DO_NOTHING} State;
 
 typedef enum kbd_game_event {LEFTKEY_DOWN, RIGHTKEY_DOWN, UPKEY_DOWN, DOWNKEY_DOWN, SPACEBAR_DOWN, NOKEY} kbd_game_event;
 
