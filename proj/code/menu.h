65e4aa3565b4b659fcd0ae01229f80ff5f45ab15
up201405840 <up201405840@8b2b798f-491d-4078-857d-234f47bdacfb>
2015-12-06 00:20:56 +0000

    include <stdint.h> disappeared

--- proj/code/menu.h
+++ proj/code/menu.h
@@ -1,6 +1,7 @@
 #ifndef __MENU_H
 #define __MENU_H
 #include "bitmap.h"
+#include <stdint.h>
 
 typedef enum main_menu_event {BUTTON1_CLICK, BUTTON2_CLICK, BUTTON3_CLICK, BUTTON4_CLICK, BUTTON5_CLICK, BUTTON6_CLICK} main_menu_event;
 typedef enum main_menu_state {BUTTON1_ABOVE, BUTTON2_ABOVE, BUTTON3_ABOVE, BUTTON4_ABOVE, BUTTON5_ABOVE, BUTTON6_ABOVE, NOACTION} main_menu_state;
@@ -8,7 +9,7 @@ typedef enum main_menu_state {BUTTON1_ABOVE, BUTTON2_ABOVE, BUTTON3_ABOVE, BUTTO
 typedef struct {
   unsigned int x,y;             /**< current sprite position */
   unsigned int width, height;   /**< sprite dimensions */
-  uint16_t* above, null;           /**< the sprite pixmap (use read_xpm()) */
+  uint16_t * above, null;           /**< the sprite pixmap (use read_xpm()) */
   unsigned int isAbove;
 } Button;
 
