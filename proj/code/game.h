39a049aeb84cff8ca4ecbb0dc44ed4044a3b115d
Jorge Vale <jorge-vale1@hotmail.com>
2015-12-06 01:01:04 +0000

    timer_handler in progress

--- proj/code/game.h
+++ proj/code/game.h
@@ -8,6 +8,8 @@ typedef enum {FALL, MOVE_RIGHT, MOVE_LEFT, ROTATE, SWAP_PIECES ,REACH_END, DO_NO
 
 typedef enum kbd_game_event {LEFTKEY_DOWN, RIGHTKEY_DOWN, UPKEY_DOWN, DOWNKEY_DOWN, SPACEBAR_DOWN, NOKEY} kbd_game_event;
 
+typedef enum timer_game_event {FALL_TICK, NO_TICK} timer_game_event;
+
 //typedef enum {} mouse_game_event;
 
 typedef struct {
@@ -17,6 +19,8 @@ typedef struct {
 	Piece * next_piece;
 	State state, last_state;
 	kbd_game_event kbd_event, last_kbd_event;
+	unsigned int fall_delay;
+	timer_game_event timer_event;
 } Game;
 
 Game * new_game(unsigned int mode);
