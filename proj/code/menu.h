ad7437a2aca73e20a96ccd6c212c9ab1a6ef950a
up201404572 <up201404572@8b2b798f-491d-4078-857d-234f47bdacfb>
2015-12-07 12:30:40 +0000

    state machine

--- proj/code/menu.h
+++ proj/code/menu.h
@@ -3,21 +3,22 @@
 #include "bitmap.h"
 #include <stdint.h>
 
-typedef enum main_menu_event {BUTTON0_CLICK, BUTTON1_CLICK, BUTTON2_CLICK, BUTTON3_CLICK, BUTTON4_CLICK, BUTTON5_CLICK} main_menu_event;
-typedef enum main_menu_state {BUTTON0_ABOVE, BUTTON1_ABOVE, BUTTON2_ABOVE, BUTTON3_ABOVE, BUTTON4_ABOVE, BUTTON5_ABOVE, NOACTION} main_menu_state;
+typedef enum main_menu_event {lEFT_CLICK, BUTTON0_ISABOVE, BUTTON1_ISABOVE, BUTTON2_ISABOVE, BUTTON3_ISABOVE, BUTTON4_ISABOVE, BUTTON5_ISABOVE, NOACTION} main_menu_event;
+
+typedef enum main_menu_state {BUTTON0_ABOVE, BUTTON1_ABOVE, BUTTON2_ABOVE, BUTTON3_ABOVE, BUTTON4_ABOVE, BUTTON5_ABOVE} main_menu_state;
 
 typedef struct {
-  unsigned int x,y;
-  unsigned int width, height;
-  uint16_t * above;
-  uint16_t * null;
-  unsigned int isAbove;
+	unsigned int x,y;
+	unsigned int width, height;
+	uint16_t * above;
+	uint16_t * null;
+	unsigned int isAbove;
 } Button;
 
 
 typedef struct {
 	Bitmap* background;
-	main_menu_state state;
+	main_menu_state state, last_state;
 	main_menu_event event;
 	Button * buttons;
 } Menu;
@@ -30,6 +31,6 @@ void draw_main_menu(Menu * menu);
 void draw_score_menu(Menu * menu);
 void update_main_menu(Menu * menu);
 void update_score_menu(Menu * menu);
-void update_main_menu_state(Menu * menu, unsigned int mouse_x, unsigned int mouse_y);
+void update_main_menu_state(Menu * menu);
 #endif /* __MENU_H */
 
