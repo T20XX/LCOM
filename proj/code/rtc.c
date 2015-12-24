ff6d246e4acf7cf1be2de2cb0cb04b8b2fc91216
Jorge Vale <jorge-vale1@hotmail.com>
2015-12-24 15:17:10 +0000

    rtc.c and rtc.h. RTC module getting and drawing time

--- proj/code/rtc.c
+++ proj/code/rtc.c
@@ -1,1 +1,118 @@
 #include "rtc.h"
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include <minix/sysutil.h>
+#include "i8042.h"
+#include "video_gr.h"
+
+static int rtc_hook_id=3;
+
+int rtc_subscribe_int() {
+	int rtc_id = BIT(rtc_hook_id);
+
+	if (sys_irqsetpolicy(RTC_IRQ, IRQ_REENABLE|IRQ_EXCLUSIVE, &rtc_hook_id) != OK)
+		return 1;
+	//if (sys_irqenable(&rtc_hook_id) != OK)
+	//	return 1;
+	return rtc_id;
+
+}
+
+int rtc_unsubscribe_int() {
+	if (sys_irqrmpolicy(&rtc_hook_id) != OK)
+		return 1;
+	//if (sys_irqdisable(&rtc_hook_id) != OK)
+	//	return 1;
+	return 0;
+}
+
+unsigned char bcd_convert(unsigned char bin)
+{
+	return ((bin >> 4) * 10) + (bin & 0x0F);
+}
+
+int rtc_current_time(unsigned long *hour, unsigned long *min, unsigned long *sec)
+{
+
+	unsigned long res;
+
+	do
+	{
+		sys_outb(RTC_ADDR_REG, RTC_REG_A);
+		sys_inb(RTC_DATA_REG, &res);
+	} while(res & 0x80);
+
+	sys_outb(RTC_ADDR_REG, RTC_SECONDS);
+	sys_inb(RTC_DATA_REG, sec);
+
+	do
+	{
+		sys_outb(RTC_ADDR_REG, RTC_REG_A);
+		sys_inb(RTC_DATA_REG, &res);
+	} while(res & 0x80);
+
+	sys_outb(RTC_ADDR_REG, RTC_MINUTES);
+	sys_inb(RTC_DATA_REG, min);
+
+	do
+	{
+		sys_outb(RTC_ADDR_REG, RTC_REG_A);
+		sys_inb(RTC_DATA_REG, &res);
+	} while(res & 0x80);
+
+
+	sys_outb(RTC_ADDR_REG, RTC_HOURS);
+	sys_inb(RTC_DATA_REG, hour);
+
+	sys_outb(RTC_ADDR_REG, RTC_REG_B);
+	sys_inb(RTC_DATA_REG, &res);
+
+	if(!(res & 0x04))
+	{
+		*sec = bcd_convert(*sec);
+		*min = bcd_convert(*min);
+		*hour = bcd_convert(*hour);
+	}
+
+	return 0;
+}
+
+
+void rtc_draw_current_time(unsigned int x, unsigned int y){
+	unsigned long hour,min,sec;
+	char temp[3];
+
+	rtc_current_time(&hour, &min, &sec);
+
+	sprintf(temp, "%d", hour);
+	if (hour < 10){
+		temp[2] = temp[1];
+		temp[1] = temp[0];
+		temp[0] = '0';
+	}
+	vg_string(temp,x,y, 2, WHITE);
+
+	x += 40;
+	vg_string(":",x,y, 2, WHITE);
+
+	x += 10;
+	sprintf(temp, "%d", min);
+	if (min < 10){
+		temp[2] = temp[1];
+		temp[1] = temp[0];
+		temp[0] = '0';
+	}
+	vg_string(temp,x,y, 2, WHITE);
+
+	x += 40;
+	vg_string(":",x,y, 2, WHITE);
+
+	x += 10;
+	sprintf(temp, "%d", sec);
+	if (sec < 10){
+		temp[2] = temp[1];
+		temp[1] = temp[0];
+		temp[0] = '0';
+	}
+	vg_string(temp,x,y, 2, WHITE);
+}
--- proj/code/rtc.h
+++ proj/code/rtc.h
@@ -1,4 +1,33 @@
 #ifndef __RTC_H
 #define __RTC_H
 
+#define RTC_REG_A 			10
+#define RTC_REG_B  			11
+#define RTC_REG_C  			12
+#define RTC_REG_D  			13
+
+#define RTC_ADDR_REG 		0x70
+#define RTC_DATA_REG 		0x71
+
+#define RTC_SECONDS 		0
+#define RTC_SECONDS_ALARM 	1
+#define RTC_MINUTES			2
+#define RTC_MINUTES_ALARM	3
+#define RTC_HOURS 			4
+#define RTC_HOURS_ALARM 	5
+#define RTC_DAY_WEEK 		6
+#define RTC_DAY_MONTH		7
+#define RTC_MONTH			8
+#define RTC_YEAR			9
+
+#define RTC_IRQ				8
+
+int rtc_subscribe_int();
+
+int rtc_unsubscribe_int();
+
+int rtc_current_time(unsigned long *hour, unsigned long *min, unsigned long *sec);
+
+void rtc_draw_current_time(unsigned int x, unsigned int y);
+
 #endif /* __RTC_H */
