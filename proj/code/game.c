5791a5cb29773636a331887af32385b25b691e80
Telmo Barros <T20XX@users.noreply.github.com>
2016-01-03 06:08:10 +0000

    more comments

--- proj/code/game.c
+++ proj/code/game.c
@@ -5,13 +5,6 @@
 #include "bitmap.h"
 #include "serial.h"
 
-//typedef struct {
-//	unsigned int x, y;
-//} Position;
-//
-//Position oneplayer_init = {.x= 500, .y = 60};			//Initial position for 1 player game mode
-//Position twoplayer_init = {.x= 100, .y = 60};			//Initial position for 2 player game mode
-
 Game * new_game(unsigned int mode){
 	Game * game = (Game*) malloc(sizeof(Game));
 	game->game_mode = mode;
@@ -27,7 +20,7 @@ Game * new_game(unsigned int mode){
 		x_next = x_board + ONE_PLAYER_RELATIVE_NEXT_PIECE_X;
 		y_next = y_board + ONE_PLAYER_RELATIVE_NEXT_PIECE_Y;
 	}
-	else if (game->game_mode == 1 || game->game_mode == 2){						//2 PLAYERS MODES
+	else if (game->game_mode == 1 || game->game_mode == 2){						//2 PLAYERS MODE
 		x_board = TWO_PLAYER_BOARD_X;
 		y_board = TWO_PLAYER_BOARD_Y;
 
@@ -63,7 +56,7 @@ Game * new_game(unsigned int mode){
 }
 
 void update_gamestate(Game * game){
-	//game->last_state = game->state;
+	//handle keyboard events
 	switch (game->kbd_event){
 
 	case NOKEY:
@@ -107,38 +100,8 @@ void update_gamestate(Game * game){
 	default:
 		break;
 	}
-	//	if (game->kbd_event == DOWNKEY_DOWN || game->timer_event == FALL_TICK){
-	//			if (can_piece_fall(game->actual_piece,&game->board)== 0)
-	//				game->state = FALL;
-	//			else if (game->actual_piece->sprite.y >= game->board.y + 2* 30)		//??
-	//				game->state = REACH_END;
-	//			else
-	//				game->state = GAME_OVER;
-	//	}else if (game->kbd_event == NOKEY && game->mouse_event == MOUSE_STOPPED){
-	//		game->state = DO_NOTHING;
-	//
-	//	}else if (game->kbd_event == LEFTKEY_DOWN || game->mouse_event == MOUSE_LEFT){
-	//		if (can_piece_move_x(game->actual_piece,&game->board,0)== 0)
-	//			game->state = MOVE_LEFT;
-	//		else
-	//			game->state = DO_NOTHING;
-	//
-	//	}else if (game->kbd_event == UPKEY_DOWN){
-	//		game->state = ROTATE;
-	//
-	//	}else if (game->kbd_event == RIGHTKEY_DOWN || game->mouse_event == MOUSE_RIGHT){
-	//		if (can_piece_move_x(game->actual_piece,&game->board,1)== 0)
-	//			game->state = MOVE_RIGHT;
-	//		else
-	//			game->state = DO_NOTHING;
-	//
-	//	} else if (game->kbd_event == SPACEBAR_DOWN){
-	//		if (game->pieces_already_swapped == 0){
-	//			game->state = SWAP_PIECES;
-	//			game->pieces_already_swapped = 1;
-	//		}
-	//	}
 
+	//handle timer events to make actual piece fall
 	if (game->timer_event == FALL_TICK){
 		if(game->state != FALL){
 			if (can_piece_fall(game->actual_piece,&game->board)== 0)
@@ -163,46 +126,37 @@ void update_game(Game * game){
 		game->actual_piece->sprite.xspeed += FACE_LENGTH;
 
 	} else if (game->state == ROTATE){
-		Piece * rotated = (Piece*) malloc(sizeof(Piece));
-
 		rotate_piece(game->actual_piece, &game->board);
-
-		//delete_piece(game->actual_piece);
-		//game->actual_piece = rotated;
-
 
 	} else if (game->state == DO_NOTHING){
 
 	} else if (game->state == SWAP_PIECES){
 		swap_pieces (game->actual_piece,game->next_piece, &game->board);
 
-		//delete_piece(&temp);
-
-		/*unsigned int * x = game->actual_piece->sprite.x;
-		game->actual_piece->sprite.x = game->next_piece->sprite.x;
-		game->next_piece->sprite.x = x;
-
-		unsigned int * y = &game->actual_piece->sprite.y;
-		&game->actual_piece->sprite.y = &game->next_piece->sprite.y;
-		&game->next_piece->sprite.y = y;*/
-
-
 	} else if (game->state == REACH_END){
 		unsigned int lines_removed;
 
+		//add actual piece to board
 		add_piece(game->actual_piece, &game->board);
+		//remove finished lines
 		lines_removed = remove_finished_lines(&game->board);
+		//makes next piece the actual piece
 		game->actual_piece = game->next_piece;
 		game->actual_piece->sprite.x = game->board.x + BOARD_RELATIVE_MIDDLE_X;
 		game->actual_piece->sprite.y = game->board.y;
 
-		game->next_piece = new_piece((game->board.x + 420),
-				(game->board.y + 90));									//substituir por constante
+		//generates next piece
+		game->next_piece = new_piece((game->board.x + ONE_PLAYER_RELATIVE_NEXT_PIECE_X),
+				(game->board.y + ONE_PLAYER_RELATIVE_NEXT_PIECE_Y));
 
+		//reset of pieces already swapped flag to allow news swapping
 		game->pieces_already_swapped = 0;
+		//increment points
 		game->points += 50;
 		game->points += (lines_removed * 250);
+		//update of lines completed
 		game->lines += lines_removed;
+		// serial sending lines completed, multiplayer mode only
 		if(game->game_mode == 1){
 			if (lines_removed == 0){
 				add_lines_received(&game->board, game->lines_received);
@@ -212,35 +166,45 @@ void update_game(Game * game){
 			game->lines_received = 0;
 		}
 	} else if (game->state == GAME_OVER){
+		//serial sending gameover info, multiplayer mode only
 		if(game->game_mode == 1){
 			serial_write_char('G');
 		}
 	}
 
+	//updates actual piece position
 	game->actual_piece->sprite.x += game->actual_piece->sprite.xspeed;
 	game->actual_piece->sprite.y += game->actual_piece->sprite.yspeed;
 
+	//sets actual piece speed to zero
 	game->actual_piece->sprite.xspeed = 0;
 	game->actual_piece->sprite.yspeed = 0;
 }
 
 void draw_game(Game * game){
 	char temp[8];
+	//draws black background
 	vg_rectangle(0,0,1024,768,BLACK);
+	//draws board
 	vg_map(game->board.map,game->board.x,game->board.y,game->board.width,game->board.height);
+	//draws actual piece
 	vg_sprite(&game->actual_piece->sprite,0);
 
+	//draws next piece
 	vg_string("NEXT",game->board.x+ONE_PLAYER_RELATIVE_NEXT_PIECE_X,game->board.y+RELATIVE_NEXT_STRING_Y, 2, WHITE);
 	vg_sprite(&game->next_piece->sprite,0);
 
+	//draws points
 	vg_string("POINTS",game->board.x+ONE_PLAYER_RELATIVE_NEXT_PIECE_X,game->board.y+RELATIVE_POINTS_STRING_Y, 2, WHITE);
 	sprintf(temp, "%d", game->points);
 	vg_string(temp,game->board.x+ONE_PLAYER_RELATIVE_NEXT_PIECE_X,game->board.y+RELATIVE_POINTS_Y, 2, WHITE);
 
+	//draws lines completed
 	vg_string("LINES",game->board.x+ONE_PLAYER_RELATIVE_NEXT_PIECE_X,game->board.y+RELATIVE_LINES_STRING_Y, 2, WHITE);
 	sprintf(temp, "%d", game->lines);
 	vg_string(temp,game->board.x+ONE_PLAYER_RELATIVE_NEXT_PIECE_X,game->board.y+RELATIVE_LINES_Y, 2, WHITE);
 
+	//draws lines completed
 	if (game->game_mode == 1){
 		vg_string("WAITING LINES",game->board.x+ONE_PLAYER_RELATIVE_NEXT_PIECE_X,game->board.y+RELATIVE_WAITING_LINES_STRING_Y, 2, WHITE);
 		sprintf(temp, "%d", game->lines_received);
@@ -249,6 +213,7 @@ void draw_game(Game * game){
 }
 
 void delete_game(Game * game){
+	//release the allocated memory
 	free(game);
 }
 
@@ -258,6 +223,7 @@ void add_piece(Piece * piece, Board * board){
 
 	board_ptr += (piece->sprite.x - board->x) + (board->width * (piece->sprite.y - board->y));
 
+	//copies actual piece map to board map at certain position
 	unsigned int i,j;
 	for(i = 0; i< piece->sprite.height; i++){
 		for(j = 0; j < piece->sprite.width;j++){
@@ -276,14 +242,16 @@ int remove_finished_lines(Board * board){
 
 	uint16_t * board_ptr = board->map;
 
-	board_ptr += (board->width * 30 * 21) + 30;				//??
+	board_ptr += (board->width * FACE_LENGTH * 21) + FACE_LENGTH;				//??
 
 	unsigned int i,j, counter;
 	for (i = 20; i > 0; i--){
 		for (j = 0; j< 10; j++){
+			//counts the number of pieces in line
 			if (*(board_ptr + j *30) != BLACK && *(board_ptr + j *30) != 0x2965)
 				counter++;
 		}
+		// remove the line by pushing upper lines down if it has 10 pieces
 		if(counter == 10){
 			uint16_t * tmp_ptr = board_ptr - (board->width * 30);
 
@@ -294,33 +262,26 @@ int remove_finished_lines(Board * board){
 				}
 			}
 			i++;
+			//increment lines removed
 			lines_removed++;
 		//} else if(counter == 0){
 			//break;
 		} else{
-			board_ptr -= (board->width * 30);				//??
+			//moves to upper line
+			board_ptr -= (board->width * FACE_LENGTH);
 		}
 		counter = 0;
 	}
+	//returns total lines removed
 	return lines_removed;
 }
 
 void rotate_piece(Piece * piece, Board * board){
-	/*Piece temp = *piece;
-	uint16_t * temp_ptr = temp.sprite.map;
-	uint16_t * piece_ptr = piece->sprite.map;*/
-
-	/*rotated->sprite.x = piece->sprite.x;
-	rotated->sprite.y = piece->sprite.y;
-	rotated->sprite.height = piece->sprite.width;
-	rotated->sprite.width = piece->sprite.height;*/
-
-	//*rotated = *piece;
-
 	uint16_t * temp_ptr;
 	temp_ptr = (uint16_t *)malloc(piece->sprite.height*piece->sprite.width * 2);
 	uint16_t * piece_ptr = piece->sprite.map;
 
+	//rotates piece map
 	unsigned int i,j;
 	for(i = 0; i < piece->sprite.width; i++){
 		piece_ptr = piece->sprite.map +(piece->sprite.width * (piece->sprite.height-1))+i;
@@ -331,9 +292,9 @@ void rotate_piece(Piece * piece, Board * board){
 		}
 	}
 
+	//dimensions reverse
 	unsigned int width = piece->sprite.width;
 	unsigned int height = piece->sprite.height;
-
 	piece->sprite.height = width;
 	piece->sprite.width = height;
 
@@ -341,6 +302,7 @@ void rotate_piece(Piece * piece, Board * board){
 	temp_ptr -= (width * height);
 	piece->sprite.map = temp_ptr;
 
+	// if piece can be placed transformations are accepted, if not we discard changes
 	if (can_piece_be_placed(piece, board) == 1){
 		piece->sprite.height = height;
 		piece->sprite.width = width;
@@ -351,50 +313,27 @@ void rotate_piece(Piece * piece, Board * board){
 	{
 		free (piece_ptr);
 	}
-
-	//	rotated->sprite.x = piece->sprite.x;
-	//		rotated->sprite.y = piece->sprite.y;
-	//	rotated->sprite.height = piece->sprite.width;
-	//	rotated->sprite.width = piece->sprite.height;
-	//
-	//	rotated->sprite.map = temp_ptr - (rotated->sprite.width*rotated->sprite.height);
-
-	//piece->sprite.width = temp.sprite.height;
-	//piece->sprite.height = temp.sprite.width;
-	//piece->sprite.map =  temp.sprite.map;
-
-	//delete_piece(piece);
 }
 
 void swap_pieces(Piece * actual, Piece * next, Board * board){
 	unsigned int actual_x = actual->sprite.x;
-	unsigned int next_x = next->sprite.x;
 	unsigned int actual_y = actual->sprite.y;
-	unsigned int next_y = next->sprite.y;
 	unsigned int width = actual->sprite.width;
 	unsigned int height = actual->sprite.height;
 
-	uint16_t * temp_ptr;
-	temp_ptr = (uint16_t *)malloc(actual->sprite.height*actual->sprite.width * 2);
 	uint16_t *actual_ptr = actual->sprite.map;
 
-	unsigned int i;
-	for(i=0; i < actual->sprite.height;i++){
-		memcpy(temp_ptr,actual_ptr,actual->sprite.width*2);
-		temp_ptr += actual->sprite.width;
-		actual_ptr += actual->sprite.width;
-	}
-
+	// makes actual piece sprite the next piece sprite
 	actual->sprite = next->sprite;
-	//actual->sprite.x=actual_x;
-	//actual->sprite.y=actual_y;
 
+	// places new actual piece in top of board
 	actual->sprite.x = board->x + BOARD_RELATIVE_MIDDLE_X;
 	actual->sprite.y = board->y;
 
+	// changes variables of next piece to certain coordinates
 	next->sprite.width = width;
 	next->sprite.height = height;
-	next->sprite.map = temp_ptr - (width*height);
+	next->sprite.map = actual_ptr;
 }
 
 int add_lines_received(Board * board, unsigned int lines){
@@ -404,12 +343,14 @@ int add_lines_received(Board * board, unsigned int lines){
 
 	unsigned int i;
 	for(i= 0; i < (22 - lines);i++){
+		//copies downer line that is always full of grey blocks
 		memcpy(board_ptr, temp_ptr, board->width * FACE_LENGTH * 2);
 		board_ptr += board->width * FACE_LENGTH;
 		temp_ptr += board->width * FACE_LENGTH;
 	}
 	temp_ptr = board->map + board->width * FACE_LENGTH * 22;
 	for(i= 0; i < lines;i++){
+		//moves downer lines up by copying them up
 			memcpy(board_ptr, temp_ptr, board->width * FACE_LENGTH * 2);
 			board_ptr += board->width * FACE_LENGTH;
 		}
