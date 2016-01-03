800cf9580b8a0f9428782931ff5349c7cec60e54
up201405840 <up201405840@8b2b798f-491d-4078-857d-234f47bdacfb>
2016-01-03 06:08:10 +0000

    more comments

--- proj/code/menu.h
+++ proj/code/menu.h
@@ -1,8 +1,16 @@
+/**
+ * This module contains all functions that handle the menu and his buttons
+ * Jorge Vale: 10%
+ * Telmo Barros: 90%
+ * Relative Weight:
+ */
 #ifndef __MENU_H
 #define __MENU_H
 #include "bitmap.h"
 #include <stdint.h>
 
+/** @name Main menu states*/
+/** @{ */
 typedef enum {
 	BUTTON0_SELECT,
 	BUTTON1_SELECT,
@@ -16,11 +24,23 @@ typedef enum {
 	HIGHSCORES,
 	INSTRUCTIONS,
 	SHUTDOWN,
-	DONOTHING} main_menu_state;
+	DONOTHING
+} main_menu_state;
+/** @} end of Maine menu states */
 
+/** @name Events of mouse related to click in main menu buttons and pass by */
+/** @{ */
 typedef enum main_menu_event {
-	LEFT_CLICK, BUTTON0_ISABOVE, BUTTON1_ISABOVE, BUTTON2_ISABOVE, BUTTON3_ISABOVE, BUTTON4_ISABOVE, BUTTON5_ISABOVE, NOACTION
+	LEFT_CLICK,
+	BUTTON0_ISABOVE,
+	BUTTON1_ISABOVE,
+	BUTTON2_ISABOVE,
+	BUTTON3_ISABOVE,
+	BUTTON4_ISABOVE,
+	BUTTON5_ISABOVE,
+	NOACTION
 } main_menu_event;
+/** @} end of Maine menu events */
 
 typedef struct {
 	unsigned int x,y;
@@ -41,13 +61,22 @@ typedef struct {
 
 
 Button new_button(const char* filename_null,const char* filename_above);
+
 Menu * new_main_menu();
+
 //Menu * new_score_menu();
+
 void draw_main_menu(Menu * menu);
+
 //void draw_score_menu(Menu * menu);
+
 void update_main_menu(Menu * menu);
+
 //void update_score_menu(Menu * menu);
+
 void update_main_menu_state(Menu * menu);
+
 void delete_main_menu(Menu * menu);
+
 #endif /* __MENU_H */
 
